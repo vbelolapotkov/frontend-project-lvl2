1758b6d55e4b76e97afbf313baf26526ace63f6e
Vasily Belolapotkov <belolapotkov.v@gmail.com>
2020-09-05 09:34:08 +0300

    Add args and format option to the command

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,5 +7,7 @@ const program = new commander.Command();
 program
   .description('Compares two configuration files and shows a difference.')
   .version(packageConfig.version)
+  .option('-f, --format [type]', 'output format')
   .helpOption('-h, --help', 'output usage information')
+  .arguments('<filepath1> <filepath2>')
   .parse(process.argv);
