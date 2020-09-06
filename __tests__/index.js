4c71255c939479a4de3d4bd748a37a7c624659f6
Vasily Belolapotkov <belolapotkov.v@gmail.com>
2020-09-06 21:12:39 +0300

    Add  absolut paths to the files in test

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,6 +1,14 @@
+/* globals fixturesPath */
+import path from 'path'
 import genDiff from '../src/index.js';
 
 test('plain json diff', () => {
   const diff = genDiff('./__fixtures__/file1.json', './__fixtures__/file2.json');
   expect(diff).toMatchSnapshot();
+
+  const diffAbs = genDiff(
+    path.resolve(fixturesPath, './file1.json'),
+    path.resolve(fixturesPath, './file2.json')
+  );
+  expect(diff).toBe(diffAbs);
 });
