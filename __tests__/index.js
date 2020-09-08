bb15efd8d5efe49463ba02315fc01ca85eadace5
Vasily Belolapotkov <belolapotkov.v@gmail.com>
2020-09-08 21:48:34 +0300

    Add failing test for plain yml diff

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,4 +1,5 @@
 /* globals fixturesPath */
+import fs from 'fs';
 import path from 'path'
 import genDiff from '../src/index.js';
 
@@ -11,4 +12,17 @@ test('plain json diff', () => {
     path.resolve(fixturesPath, './file2.json')
   );
   expect(diff).toBe(diffAbs);
+});
+
+test('plain yaml diff', () => {
+  const expectedDiff = fs.readFileSync(path.resolve(fixturesPath, './plain_diff.txt'), 'utf-8');
+
+  const diff = genDiff( './__fixtures__/file1.yml', './__fixtures__/file2.yml' );
+  expect(diff).toEqual(expectedDiff);
+
+  const diffAbs = genDiff(
+    path.resolve(fixturesPath, './file1.yml'),
+    path.resolve(fixturesPath, './file2.yml')
+  );
+  expect(diffAbs).toEqual(expectedDiff);
 });
