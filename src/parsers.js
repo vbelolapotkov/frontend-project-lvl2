bcfff0b4262e141e3de4b60ad462176386d89a0e
Vasily Belolapotkov <belolapotkov.v@gmail.com>
2020-09-08 22:42:44 +0300

    Add support for ini files

--- src/parsers.js
+++ src/parsers.js
@@ -1,16 +1,18 @@
 import fs from 'fs';
 import path from 'path';
 import yaml from 'js-yaml';
+import ini from 'ini';
 
 const configParsers = {
   '.yml': yaml.safeLoad,
+  '.ini': ini.parse,
   default: JSON.parse,
 };
 
 export default function parseConfigFile(filepath) {
   const absPath = path.resolve(process.cwd(), filepath);
   const format = path.extname(absPath);
-  const fileContent = fs.readFileSync(absPath);
+  const fileContent = fs.readFileSync(absPath, 'utf-8');
   const parse =
     typeof configParsers[format] === 'function'
       ? configParsers[format]
