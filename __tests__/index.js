d3ed0cae67f08e3e96a7aab7e646ea304e05d46f
Vasily Belolapotkov <belolapotkov.v@gmail.com>
2020-09-10 10:02:35 +0300

    Add ugly solution for nested json diff

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,31 +1,49 @@
 /* globals fixturesPath */
-import fs from 'fs';
+import fs, { read } from 'fs';
 import path from 'path'
 import genDiff from '../src/index.js';
 
 const formats = ['json', 'yml', 'ini'];
+
+const readFixtureFile = fileName => fs.readFileSync(path.resolve(fixturesPath, fileName), 'utf-8');
+
 describe('genDiff', () => {
-  let expectedDiff;
+  let plaindDiff;
+  let nestedDiff;
 
   beforeAll(() => {
-    expectedDiff = fs.readFileSync(
-      path.resolve(fixturesPath, './plain_diff.txt'),
-      'utf-8'
-    );
+    plaindDiff = readFixtureFile('plain_diff.txt');
+    nestedDiff = readFixtureFile('nested_diff.txt')
   });
 
-  test.each(formats)('diff for plain %s file', (format) => {
+
+  test.each(formats)('diff for plain %s files', (format) => {
     const diff = genDiff(
       `./__fixtures__/file1.${format}`,
       `./__fixtures__/file2.${format}`
     );
 
-    expect(diff).toBe(expectedDiff);
+    expect(diff).toBe(plaindDiff);
 
     const diffAbs = genDiff(
       path.resolve(fixturesPath, `./file1.${format}`),
       path.resolve(fixturesPath, `./file2.${format}`)
     );
-    expect(diffAbs).toBe(expectedDiff);
+    expect(diffAbs).toBe(plaindDiff);
+  });
+
+  test('diff for nested json files', () => {
+    const diff = genDiff(
+      './__fixtures__/file3.json',
+      './__fixtures__/file4.json'
+    );
+
+    expect(diff).toBe(nestedDiff);
+
+    const diffAbs = genDiff(
+      path.resolve(fixturesPath, './file3.json'),
+      path.resolve(fixturesPath, './file4.json')
+    );
+    expect(diffAbs).toBe(nestedDiff);
   });
 });
--- src/index.js
+++ src/index.js
@@ -1,33 +1,124 @@
+import _ from 'lodash';
 import readConfigFromFile from './parsers.js';
 
-function genObjectsDiff(obj1, obj2) {
-  const uniqKeys = new Set([...Object.keys(obj1), ...Object.keys(obj2)]);
-  const sortedKeys = Array.from(uniqKeys.values()).sort();
+function isObject (variable) {
+  return  typeof variable === 'object' && variable !== null && ! Array.isArray(variable);
+}
 
-  const diffLines = ['{'];
-  sortedKeys.forEach((key) => {
-    if (obj1[key] === obj2[key]) {
-      diffLines.push(`    ${key}: ${obj1[key]}`);
-      return;
+function stylish(diff) {
+  const indentStep = '  ';
+  const diffTypeSymbols = {
+    added: '+',
+    removed: '-',
+    same: ' ',
+  };
+  const getLines = (diff, indent) => {
+    const diffLines = []
+    let line = indent;
+
+    const diffTypeStr = diff.diffType && diffTypeSymbols[diff.diffType] ? `${diffTypeSymbols[diff.diffType]} ` : ''
+
+    line += diffTypeStr;
+
+    if ( diff.value !== undefined && !isObject(diff.value) ) {
+      line += `${diff.key}: ${diff.value}`;
+      diffLines.push(line);
+      return diffLines;
     }
 
-    if (obj1[key] !== undefined) {
-      diffLines.push(`  - ${key}: ${obj1[key]}`);
+    if (diff.key) {
+      line+= `${diff.key}: `
+    }
+    line += '{';
+    diffLines.push(line);
+
+    const nextIndent = _.padStart(
+      indent,
+      indent.length + indentStep.length + diffTypeStr.length
+    );
+
+    if (isObject(diff.value)) {
+      diffLines.push(
+        ...Object.keys(diff.value).flatMap(key => getLines({ key, value: diff.value[key], diffType: 'same' }, nextIndent))
+      );
     }
 
-    if (obj2[key] !== undefined) {
-      diffLines.push(`  + ${key}: ${obj2[key]}`);
+    if (diff.children && diff.children.length > 0) {
+      diffLines.push(
+        ...diff.children.flatMap((child) => getLines(child, nextIndent))
+      );
     }
-  });
 
-  diffLines.push('}');
-  return diffLines;
+    const closingIndent = _.padStart(
+      indent,
+      indent.length + diffTypeStr.length
+    );
+    diffLines.push(`${closingIndent}}`);
+
+    return diffLines;
+  }
+
+  const lines = getLines(diff, '');
+  return lines.join('\n');
+}
+
+function genDiffTree(obj1, obj2) {
+  const getObjectsDiff = (obj1, obj2) => {
+    const objectsDiff = [];
+    const uniqKeys = new Set([...Object.keys(obj1), ...Object.keys(obj2)]);
+    const sortedKeys = Array.from(uniqKeys.values()).sort();
+    sortedKeys.forEach((key) => {
+      const value1 = obj1[key];
+      const value2 = obj2[key];
+      if (value1 === value2) {
+        objectsDiff.push({
+          key,
+          value: value1,
+          diffType: 'same',
+        });
+        return;
+      }
+
+      if (isObject(value1) && isObject(value2)) {
+        objectsDiff.push({
+          key,
+          diffType: 'same',
+          children: getObjectsDiff(value1, value2),
+        })
+      } else {
+        if (value1 !== undefined) {
+          objectsDiff.push({
+            key,
+            value: value1,
+            diffType: 'removed',
+          });
+        }
+
+        if (value2 !== undefined) {
+          objectsDiff.push({
+            key,
+            value: value2,
+            diffType: 'added',
+          });
+        }
+      }
+    });
+
+
+    return objectsDiff;
+  };
+
+  const diff = {
+    children: getObjectsDiff(obj1, obj2)
+  }
+
+  return diff;
 }
 
 export default function genDiff(filepath1, filepath2) {
   const config1 = readConfigFromFile(filepath1);
   const config2 = readConfigFromFile(filepath2);
 
-  const diffLines = genObjectsDiff(config1, config2);
-  return diffLines.join('\n');
+  const diff = genDiffTree(config1, config2);
+  return stylish(diff);
 }
