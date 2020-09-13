2e19e7281107289a955e9c231655acf8fcf7001f
Vasily Belolapotkov <belolapotkov.v@gmail.com>
2020-09-13 21:15:08 +0300

    Fix stylish formatter

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,68 +1,57 @@
 import _ from 'lodash';
 
-// Node ES modules does not support named exports from lodash.
-const { isPlainObject, padStart } = _;
 
-const composeDiffTypeStr = (diffType) => {
-  const diffTypeSymbols = {
-    added: '+',
-    removed: '-',
-    same: ' ',
-  };
-
-  return diffType && diffTypeSymbols[diffType]
-    ? `${diffTypeSymbols[diffType]} `
-    : '';
+const getNextIndent = (indent, key) => {
+  const indentStep = '  ';
+  return key ? `${indent}  ${indentStep}` : indent + indentStep;
 };
 
-const composeDiffLines = (diff, indent) => {
-  const indentStep = '  ';
-  const diffTypeStr = composeDiffTypeStr(diff.diffType);
+const composeDiffValueLines = (indent, key, value, diffSymbol = ' ') => {
+  return _.isPlainObject(value)
+    ? [
+        `${indent}${diffSymbol} ${key}: {`,
+        ...Object.keys(value).flatMap((objectKey) =>
+          composeDiffValueLines(
+            getNextIndent(indent, objectKey),
+            objectKey,
+            value[objectKey],
+          )
+        ),
+        `${indent}  }`,
+      ]
+    : [`${indent}${diffSymbol} ${key}: ${value}`];
+};
 
-  let line = indent + diffTypeStr;
-
-  if (diff.value !== undefined && !isPlainObject(diff.value)) {
-    line += `${diff.key}: ${diff.value}`;
-    return [line];
-  }
-
-  const diffLines = [];
-
-  if (diff.key) {
-    line += `${diff.key}: `;
-  }
-  line += '{';
-  diffLines.push(line);
-
-  const nextIndent = padStart(
-    indent,
-    indent.length + diffTypeStr.length + indentStep.length
-  );
-
-  if (isPlainObject(diff.value)) {
-    diffLines.push(
-      ...Object.keys(diff.value).flatMap((key) =>
-        composeDiffLines(
-          { key, value: diff.value[key], diffType: 'same' },
-          nextIndent
-        )
-      )
-    );
-  }
-
+const composeDiffLines = (diff, indent = '') => {
   if (diff.children && diff.children.length > 0) {
-    diffLines.push(
-      ...diff.children.flatMap((child) => composeDiffLines(child, nextIndent))
-    );
+    const keyString = diff.key ? `  ${diff.key}: ` : '';
+    const nextIndent = getNextIndent(indent, diff.key);
+    return [
+      `${indent}${keyString}{`,
+      ...diff.children.flatMap((child) => composeDiffLines(child, nextIndent)),
+      `${indent}${keyString.length ? '  ' : ''}}`,
+    ];
   }
 
-  const closingIndent = padStart(indent, indent.length + diffTypeStr.length);
-  diffLines.push(`${closingIndent}}`);
+  if (diff.prevValue === undefined && diff.nextValue !== undefined) {
+    return composeDiffValueLines(indent, diff.key, diff.nextValue, '+');
+  }
 
-  return diffLines;
+  if (diff.prevValue !== undefined && diff.nextValue === undefined) {
+    return composeDiffValueLines(indent, diff.key, diff.prevValue, '-');
+  }
+
+  if (diff.prevValue !== diff.nextValue) {
+    return [
+      ...composeDiffValueLines(indent, diff.key, diff.prevValue, '-'),
+      ...composeDiffValueLines(indent, diff.key, diff.nextValue, '+'),
+    ];
+  }
+
+  return `${indent}  ${diff.key}: ${diff.prevValue}`;
 };
 
 export default function stylish(diff) {
-  const lines = composeDiffLines(diff, '');
+  const lines = composeDiffLines(diff);
   return lines.join('\n');
 }
