c70cb5429708197635ccdb5a409015b44ae56334
Vasily Belolapotkov <belolapotkov.v@gmail.com>
2020-09-13 14:30:08 +0300

    Refactor stylish formatter

--- src/formatters.js
+++ src/formatters.js
@@ -3,67 +3,64 @@ import _ from 'lodash';
 // Node ES modules does not support named exports from lodash.
 const { isPlainObject, padStart } = _;
 
-export default function stylish(diff) {
-  const indentStep = '  ';
+const composeDiffTypeStr = diffType => {
   const diffTypeSymbols = {
     added: '+',
     removed: '-',
     same: ' ',
   };
-  const getLines = (diff, indent) => {
-    const diffLines = [];
-    let line = indent;
 
-    const diffTypeStr =
-      diff.diffType && diffTypeSymbols[diff.diffType]
-        ? `${diffTypeSymbols[diff.diffType]} `
-        : '';
+  return diffType && diffTypeSymbols[diffType]
+    ? `${diffTypeSymbols[diffType]} `
+    : '';
 
-    line += diffTypeStr;
+}
 
-    if (diff.value !== undefined && !isPlainObject(diff.value)) {
-      line += `${diff.key}: ${diff.value}`;
-      diffLines.push(line);
-      return diffLines;
-    }
+const composeDiffLines = (diff, indent) => {
+  const indentStep = '  ';
+  const diffTypeStr = composeDiffTypeStr(diff.diffType);
 
-    if (diff.key) {
-      line += `${diff.key}: `;
-    }
-    line += '{';
-    diffLines.push(line);
+  let line = indent + diffTypeStr;
 
-    const nextIndent = padStart(
-      indent,
-      indent.length + indentStep.length + diffTypeStr.length
+  if (diff.value !== undefined && !isPlainObject(diff.value)) {
+    line += `${diff.key}: ${diff.value}`;
+    return [line];
+  }
+
+  const diffLines = [];
+
+  if (diff.key) {
+    line += `${diff.key}: `;
+  }
+  line += '{';
+  diffLines.push(line);
+
+  const nextIndent = padStart(
+    indent,
+    indent.length + diffTypeStr.length + indentStep.length
+  );
+
+  if (isPlainObject(diff.value)) {
+    diffLines.push(
+      ...Object.keys(diff.value).flatMap((key) =>
+        composeDiffLines({ key, value: diff.value[key], diffType: 'same' }, nextIndent)
+      )
     );
+  }
 
-    if (isPlainObject(diff.value)) {
-      diffLines.push(
-        ...Object.keys(diff.value).flatMap((key) =>
-          getLines(
-            { key, value: diff.value[key], diffType: 'same' },
-            nextIndent
-          )
-        )
-      );
-    }
-
-    if (diff.children && diff.children.length > 0) {
-      diffLines.push(
-        ...diff.children.flatMap((child) => getLines(child, nextIndent))
-      );
-    }
-
-    const closingIndent = padStart(
-      indent,
-      indent.length + diffTypeStr.length
+  if (diff.children && diff.children.length > 0) {
+    diffLines.push(
+      ...diff.children.flatMap((child) => composeDiffLines(child, nextIndent))
     );
-    diffLines.push(`${closingIndent}}`);
+  }
 
-    return diffLines;
-  };
+  const closingIndent = padStart(indent, indent.length + diffTypeStr.length);
+  diffLines.push(`${closingIndent}}`);
 
-  const lines = getLines(diff, '');
+  return diffLines;
+};
+
+export default function stylish(diff) {
+  const lines = composeDiffLines(diff, '');
   return lines.join('\n');
 }
