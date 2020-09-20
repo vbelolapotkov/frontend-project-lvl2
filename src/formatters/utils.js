4b278b1d64b667371aa3fbef18f72c24c78d1c30
Vasily Belolapotkov <belolapotkov.v@gmail.com>
2020-09-20 16:35:57 +0300

    Refactor flatten util
    Hide path accumulator used by internal iterator from util signature.

--- src/formatters/utils.js
+++ src/formatters/utils.js
@@ -1,12 +1,16 @@
 /* eslint-disable import/prefer-default-export */
 
-export function flatten(diff, path = []) {
-  const currentPath = diff.key ? [...path, diff.key] : path;
+export function flatten(diffTree) {
+  const iter = (diff, path) => {
+    const currentPath = diff.key ? [...path, diff.key] : path;
 
-  if (diff.children && diff.children.length > 0) {
-    return [...diff.children.flatMap((child) => flatten(child, currentPath))];
+    if (diff.children && diff.children.length > 0) {
+      return [...diff.children.flatMap((child) => iter(child, currentPath))];
+    }
+
+    const pathKey = currentPath.join('.');
+    return [{ ...diff, key: pathKey }];
   }
 
-  const pathKey = currentPath.join('.');
-  return [{ ...diff, key: pathKey }];
+  return iter(diffTree, []);
 }
