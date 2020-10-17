69ac1246ca76fdaf7c352c607edac21f2fe4858e
Vasily Belolapotkov <belolapotkov.v@gmail.com>
2020-10-17 15:54:33 +0300

    Update  checks order in diff tree generator

--- src/diff-tree.js
+++ src/diff-tree.js
@@ -1,51 +1,48 @@
 import _ from 'lodash';
 
 // Node ES modules does not support named exports from lodash.
-const { isPlainObject, has } = _;
+const { isPlainObject, has, isEqual } = _;
 
 const getObjectsDiff = (obj1, obj2) => {
   const sortedKeys = _.sortBy(_.union(Object.keys(obj1), Object.keys(obj2)));
   return sortedKeys.flatMap((key) => {
-    const value1 = obj1[key];
-    const value2 = obj2[key];
-
-    if (isPlainObject(value1) && isPlainObject(value2)) {
-      return {
-        key,
-        type: 'nested',
-        children: getObjectsDiff(value1, value2),
-      };
-    }
-
-    if (value1 === value2) {
-      return {
-        key,
-        type: 'unchanged',
-        value: value2,
-      };
-    }
-
-    if (has(obj1, key) && !has(obj2, key)) {
-      return {
-        key,
-        type: 'deleted',
-        prevValue: value1,
-      };
-    }
-
-    if (!has(obj1, key) && has(obj2, key)) {
+    if (!has(obj1, key)) {
       return {
         key,
         type: 'added',
-        value: value2,
+        value: obj2[key],
+      };
+    }
+
+    if (!has(obj2, key)) {
+      return {
+        key,
+        type: 'deleted',
+        prevValue: obj1[key],
+      };
+    }
+
+    if (isPlainObject(obj1[key]) && isPlainObject(obj2[key])) {
+      return {
+        key,
+        type: 'nested',
+        children: getObjectsDiff(obj1[key], obj2[key]),
+      };
+    }
+
+    if (isEqual(obj1[key], obj2[key])) {
+      return {
+        key,
+        type: 'unchanged',
+        value: obj2[key],
       };
     }
 
     return {
       key,
       type: 'changed',
-      prevValue: value1,
-      value: value2,
+      prevValue: obj1[key],
+      value: obj2[key],
     };
   });
 };
